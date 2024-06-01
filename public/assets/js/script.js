47546da62de930339dfab6f304d0ced0682e9464
SREANSOPHEA <sreansophea2105@gmail.com>
2024-06-01 09:54:19 +0700

    updated

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -38,20 +38,36 @@ function addPurchase(){
         if(qty <1 ){
             alert('Quantity must greater than 0');
         }else{
-            var div = document.createElement('div');
-            div.className = "card col-3";
-            div.id = "purchase"+id;
-            document.getElementById('purchaseData').appendChild(div);
-    
-            var input = document.createElement('input');
-            input.value = name;
-            input.name = "name[]";
-            document.getElementById('purchase'+id).appendChild(input);
+            var check = document.getElementById('qty'+id);
+            if(check){
+                var old_qty = document.getElementById('qty'+id).value;
+                document.getElementById('qty'+id).value = parseFloat(old_qty)+parseFloat(qty);
+            }else{
+                var div = document.createElement('div');
+                div.className = "card col-3 p-2";
+                div.id = "purchase"+id;
+                document.getElementById('purchaseData').appendChild(div);
+        
+                var div = document.createElement('div');
+                div.className = "dropdown-menu";
+                div.id = "menu"+id;
+                document.getElementById('purchaseData').appendChild(div);
 
-            var img = document.createElement('img');
-            img.src = "http://localhost:8000/images/"+image;
-            document.getElementById('purchase'+id).appendChild(img);
+                var input = document.createElement('input');
+                input.value = name;
+                input.name = "name[]";
+                document.getElementById('purchase'+id).appendChild(input);
     
+                var input = document.createElement('input');
+                input.value = qty;
+                input.name = "qty[]";
+                input.id = "qty"+id;
+                document.getElementById('purchase'+id).appendChild(input);
+    
+                var img = document.createElement('img');
+                img.src = "http://localhost:8000/images/"+image;
+                document.getElementById('purchase'+id).appendChild(img);
+            }
         }
         document.getElementById('selectQty').value = "";
     }
