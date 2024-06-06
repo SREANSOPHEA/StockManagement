41c85cb5b335b9046993a211d799794820bc6d10
SREANSOPHEA <sreansophea2105@gmail.com>
2024-06-06 09:14:09 +0700

    updated

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" href="{{asset('assets/css/user.css')}}">
 </head>
 <body>
-    <div class="header" style="height: 10vh">
+    <div class="header">
         <ul>
-            <li><a href="/">Logo</a></li>
+            <li class="logo"><a href="/">Logo</a></li>
             <li class="menu"><a href="/home">Home</a></li>
             <li class="menu"><a href="/shop">Shop</a></li>
             <li class="menu"><a href="/about-us">About us</a></li>
         </ul>
     </div>
+    @yield('content')
 </body>
 </html>
