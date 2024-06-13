1ad9d4ee4258b60a50a26b9e772db61c9c883996
sreanousophea <sreansophea2105@gmail.com>
2024-06-13 21:37:15 +0700

    updated

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -6,6 +6,33 @@
     }
 </style>
 @section('content')
-    <img src="{{asset('images/beltei.png')}}" width="100px">
+    <div class="w-100 row pt-3">
+        <div class="col-md-6 text-center p-3">
+            <div class="w-75">
+                <h3>Lecturer</h3>
+                <img src="{{asset('images/teacher.jpg')}}" class="w-75">
+                <h4>Mr. HANG Sovann</h4>
+            </div>
+        </div>
+        <div class="col-md-6 text-center p-3">
+            <div class="bg-dark text-light rounded w-100">
+                <h3>Group Member</h3>
+                {{-- <img src="{{asset('images/teacher.jpg')}}" class="w-50"> --}}
+                <h4 id="stu_name"></h4>
+            </div>
+        </div>
+    </div>
     
+    {{-- <script>
+        var stu_name = ['Mr. SREAN Ousophea','Mr. KIM Sovannara'];
+        let i = 0;
+        window.addEventListener('load',slide());
+        function slide(){
+            var name = document.getElementById('stu_name');
+            name.innerHTML = stu_name[i];
+            if(i<stu_name.length-1) i++;
+            else i=0;
+            settimeout(slide,1000);
+        }
+    </script> --}}
 @endsection
--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{asset('assets/css/user.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="header">
